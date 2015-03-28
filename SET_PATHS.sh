7b63f177d5be474084ea10cd8cebf7104c87be96
Rudolf Cardinal <rudolf@pobox.com>
2015-03-28 22:19:39 +0000

    JDBC

--- SET_PATHS.sh
+++ SET_PATHS.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-pythonpathadd() {
+add_pythonpath() {
     # http://superuser.com/questions/39751
     if [ -d "$1" ] && [[ ":$PYTHONPATH:" != *":$1:"* ]]; then
         export PYTHONPATH="${PYTHONPATH:+"$PYTHONPATH:"}$1"
+    fi
+}
+
+add_javaclasspath() {
+    # doesn't have to be a directory
+    if [[ ":$CLASSPATH:" != *":$1:"* ]]; then
+        export CLASSPATH="${CLASSPATH:+"$CLASSPATH:"}$1"
     fi
 }
 
@@ -14,6 +21,9 @@ if [ "$0" != "bash" ] && [ "$0" != "-bash" ]; then
     exit 1
 fi
 
-pythonpathadd $DIR/pythonlib
-pythonpathadd $DIR/anon_modules
+add_pythonpath $DIR/pythonlib
+add_pythonpath $DIR/anon_modules
+add_javaclasspath /usr/share/java/mysql.jar
+add_javaclasspath $DIR/sqljdbc_4.1/enu/sqljdbc41.jar
 echo "New PYTHONPATH should be: $PYTHONPATH"
+echo "New (Java) CLASSPATH should be: $CLASSPATH"
