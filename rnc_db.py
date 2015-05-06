3a22af0d921f30848ea347d2f6486038bd4cb68e
Rudolf Cardinal <rudolf@pobox.com>
2015-05-06 15:30:31 +0100

    spaces in fieldnames

--- rnc_db.py
+++ rnc_db.py
@@ -418,7 +418,9 @@ def debug_query_result(rows):
 # SQL types and validation
 # =============================================================================
 
-REGEX_INVALID_TABLE_FIELD_CHARS = re.compile("[^a-zA-Z0-9_ ]")
+# REGEX_INVALID_TABLE_FIELD_CHARS = re.compile("[^a-zA-Z0-9_ ]")
+REGEX_INVALID_TABLE_FIELD_CHARS = re.compile("[^\x20-\x7E]")
+# ... SQL Server is very liberal!
 
 
 def is_valid_field_name(f):
