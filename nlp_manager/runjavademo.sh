5d58d9d97b2b77c90d70d36a89bc6e734e12d7c3
Rudolf Cardinal <rudolf@pobox.com>
2015-06-24 16:48:10 +0100

    java demo

--- nlp_manager/runjavademo.sh
+++ nlp_manager/runjavademo.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+BUILDDIR=$DIR/compiled_nlp_classes
 GATEDIR=~/GATE_Developer_8.0
+GATEJAR="$GATEDIR/bin/gate.jar"
+GATELIBJARS="$GATEDIR/lib/*"
+APPFILE="$GATEDIR/plugins/ANNIE/ANNIE_with_defaults.gapp"
 
-java -classpath $DIR/compiled_nlp_classes:$GATEDIR/bin/gate.jar:$GATEDIR/lib/* \
-    CamAnonGatePipeline \
-    -g $GATEDIR/plugins/ANNIE/ANNIE_with_defaults.gapp \
-    -a Person -a Location -it END -ot END -v -v
+java -classpath $BUILDDIR:$GATEJAR:$GATELIBJARS CamAnonGatePipeline -g $APPFILE -a Person -a Location -it END -ot END -v -v
