b300f8a4ca23999bc6591702559599ad37407b12
Rudolf Cardinal <rudolf@pobox.com>
2015-06-24 16:26:06 +0100

    restructured directories

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# anonymise
+# crate
+Clinical Records Anonymisation and Text Extraction (CRATE)
 Anonymise relational databases and operate a GATE natural language processing (NLP) pipeline
--- launch_makedata.sh
+++ launch_makedata.sh
@@ -2,5 +2,5 @@
 # http://stackoverflow.com/questions/4824590/propagate-all-arguments-in-a-bash-shell-script
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-export PYTHONPATH=$PYTHONPATH:$DIR/pythonlib
-$DIR/makedata.py "$@"
+export PYTHONPATH=$PYTHONPATH:$DIR
+$DIR/make_demo_database/make_demo_database.py "$@"
