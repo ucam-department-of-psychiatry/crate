6c262d5ba1fbd18cb61e75ee1e9890b07977a94f
Rudolf Cardinal <rudolf@pobox.com>
2015-11-26 19:44:00 +0000

    toolfix

--- tools/PUSH_EVERYTHING.sh
+++ tools/PUSH_EVERYTHING.sh
@@ -3,7 +3,7 @@
 # Script to push to master repositories
 
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-cd $DIR
+cd $DIR/..
 git push egret master
 git push github master
 git subtree push --prefix=pythonlib github-pythonlib-subtree master
