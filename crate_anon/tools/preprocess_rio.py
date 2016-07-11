fbdba0d71858c48ac8d7cfb2ecc24a6eb726894d
Rudolf Cardinal <rudolf@pobox.com>
2016-07-11 12:47:54 +0100

    rio preprocess

--- crate_anon/tools/preprocess_rio.py
+++ crate_anon/tools/preprocess_rio.py
@@ -50,6 +50,18 @@ RCEP_COL_MANGLED_KEY = "Document_ID"
 #   including a column called, ironically, "Unique_Key".
 # - Therefore, we could either ditch the key entirely, or just use a non-UNIQUE
 #   index (and call it "key" not "pk").
+# AND THEN... In Client_Family, we have Document_ID values like
+#   773577794_1000000_1000001
+#   ^^^^^^^^^ ^^^^^^^ ^^^^^^^
+#   table ID  RiO#    Family member's RiO#
+#
+#   ... there is no unique ID. And we don't need the middle part as we already
+#   have Client_ID. So this is not very useful. We could mangle out the second
+#   and subsequent '_' characters to give a unique number here, which would
+#   meaning having PK as BIGINT not INTEGER.
+# - SQL Server's ROW_NUMBER() relates to result sets.
+# - However, ADD pkname INT IDENTITY(1, 1) works beautifully and
+#   autopopulates existing tables.
 
 # CPFT hacks (RiO tables added to RCEP output):
 CPFT_RCEP_TABLE_FULL_PROGRESS_NOTES = "Progress_Notes_II"
@@ -64,11 +76,13 @@ DEFAULT_GEOG_COLS = [
 ]
 
 # Columns added:
-CRATE_COL_PK = "pk"  # OK for RCEP + RiO
+CRATE_COL_PK = "crate_pk"
 # Do NOT use 'id'; this appears in RiO ClientAlternativeId /
-# RCEP Client_Alternative_ID.
-CRATE_COL_RIO_NUMBER = "rio_number"  # OK for RCEP + RiO
-CRATE_COL_NHS_NUMBER = "nhs_number_int"  # OK for RCEP + RiO
+# RCEP Client_Alternative_ID. "pk" is OK for RCEP + RiO, but clarity is good
+CRATE_COL_RIO_NUMBER = "crate_rio_number"
+# "rio_number" is OK for RCEP + RiO, but clarity is good
+CRATE_COL_NHS_NUMBER = "crate_nhs_number_int"
+# "nhs_number_int" is OK for RCEP + RiO, but again...
 # For RCEP, in SQL Server, check existing columns with:
 #   USE database;
 #   SELECT column_name, table_name
@@ -76,14 +90,18 @@ CRATE_COL_NHS_NUMBER = "nhs_number_int"  # OK for RCEP + RiO
 #       WHERE column_name = 'something';
 # For RiO, for now, check against documented table structure.
 
+# For progress notes:
+CRATE_COL_MAX_SUBNUM = "crate_max_subnum_for_notenum"
+CRATE_COL_LAST_NOTE = "crate_last_note_in_edit_chain"
+
 # Indexes added:
 CRATE_IDX_PK = "crate_idx_pk"  # for any patient table
 CRATE_IDX_RIONUM = "crate_idx_rionum"  # for any patient table
 CRATE_IDX_RIONUM_NOTENUM = "crate_idx_rionum_notenum"  # for Progress Notes
 
 # Views added:
-VIEW_PROGRESS_NOTES_CURRENT = "progress_notes_current"  # OK for RCEP + RiO
-VIEW_ADDRESS_WITH_GEOGRAPHY = "client_address_with_geography"  # OK, RCEP + RiO
+VIEW_PROGRESS_NOTES_CURRENT = "crate_progress_notes_current"
+VIEW_ADDRESS_WITH_GEOGRAPHY = "crate_client_address_with_geography"
 
 
 # =============================================================================
@@ -118,11 +136,11 @@ def execute(engine, args, sql):
         engine.execute(sql)
 
 
-def add_columns_if_absent(engine, args, table, name_coltype_dict):
-    column_names = table.columns.keys()
+def add_columns(engine, args, table, name_coltype_dict):
+    existing_column_names = get_column_names(engine, table=None, to_lower=True)
     column_defs = []
     for name, coltype in name_coltype_dict.items():
-        if name not in column_names:
+        if name.lower() not in existing_column_names:
             column_defs.append("{} {}".format(name, coltype))
         else:
             log.debug("Table '{}': column '{}' already exists; not "
@@ -147,7 +165,21 @@ def add_columns_if_absent(engine, args, table, name_coltype_dict):
         """.format(tablename=table.name, column_def=column_def))
 
 
-def add_index_if_absent(engine, args, table, indexdictlist):
+def drop_columns(engine, args, table, column_names):
+    existing_column_names = get_column_names(engine, table=None, to_lower=True)
+    for name in column_names:
+        if name.lower() not in existing_column_names:
+            log.debug("Table '{}': column '{}' does not exist; not "
+                      "dropping".format(table.name, name))
+        else:
+            sql = "ALTER TABLE {t} DROP COLUMN {c}".format(t=table.name,
+                                                           c=name)
+            # SQL Server: http://www.techonthenet.com/sql_server/tables/alter_table.php  # noqa
+            # MySQL: http://dev.mysql.com/doc/refman/5.7/en/alter-table.html
+            execute(engine, args, sql)
+
+
+def add_indexes(engine, args, table, indexdictlist):
     existing_index_names = get_index_names(engine, table=table, to_lower=True)
     for idxdefdict in indexdictlist:
         index_name = idxdefdict['index_name']
@@ -167,6 +199,25 @@ def add_index_if_absent(engine, args, table, indexdictlist):
         else:
             log.debug("Table '{}': index '{}' already exists; not "
                       "adding".format(table.name, index_name))
+
+
+def drop_indexes(engine, args, table, index_names):
+    existing_index_names = get_index_names(engine, table=table, to_lower=True)
+    for index_name in index_names:
+        if index_name.lower() not in existing_index_names:
+            log.debug("Table '{}': index '{}' does not exist; not "
+                      "dropping".format(table.name, index_name))
+        else:
+            log.info("Table '{}': dropping index '{}'".format(table.name,
+                                                              index_name))
+            if engine.dialect.name == 'mysql':
+                sql = "ALTER TABLE {t} DROP INDEX {i}".format(t=table.name,
+                                                              i=index_name)
+            elif engine.dialect.name == 'mssql':
+                sql = "DROP INDEX {t}.{i}".format(t=table.name, i=index_name)
+            else:
+                assert False, "Unknown dialect: {}".format(engine.dialect.name)
+            execute(engine, args, sql)
 
 
 def get_column_names(engine, tablename=None, table=None, to_lower=False,
@@ -216,6 +267,32 @@ def ensure_columns_present(engine, table=None, tablename=None,
             die("Column '{}' missing from table '{}'".format(col, tablename))
 
 
+def create_view(engine, args, viewname, select_sql):
+    # MySQL has CREATE OR REPLACE VIEW.
+    # SQL Server doesn't: http://stackoverflow.com/questions/18534919
+    if engine.dialect.name == 'mysql':
+        sql = "CREATE OR REPLACE VIEW {viewname} AS {select_sql}".format(
+            viewname=viewname,
+            select_sql=select_sql,
+        )
+        execute(engine, args, sql)
+    else:
+        drop_view(engine, args, viewname)
+        sql = "CREATE VIEW {viewname} AS {select_sql}".format(
+            viewname=viewname,
+            select_sql=select_sql,
+        )
+        execute(engine, args, sql)
+
+
+def drop_view(engine, args, viewname):
+    sql = "DROP VIEW IF EXISTS {viewname}".format(viewname=viewname)
+    # From SQL Server 2008:
+    #   https://msdn.microsoft.com/en-us/library/ms173492.aspx
+    # MySQL: http://dev.mysql.com/doc/refman/5.7/en/drop-view.html
+    execute(engine, args, sql)
+
+
 # =============================================================================
 # Generic table processors
 # =============================================================================
@@ -225,8 +302,12 @@ def process_patient_table(table, engine, args):
     # -------------------------------------------------------------------------
     # Add pk and rio_number columns, if not present
     # -------------------------------------------------------------------------
-    add_columns_if_absent(engine, args, table, {
-        CRATE_COL_PK: 'INTEGER',
+    if args.rio:
+        pktype = 'INTEGER NOT NULL'
+    else:  # RCEP
+        pktype = 'INTEGER IDENTITY(1, 1) NOT NULL'
+    add_columns(engine, args, table, {
+        CRATE_COL_PK: pktype,
         CRATE_COL_RIO_NUMBER: 'INTEGER',
     })
     # -------------------------------------------------------------------------
@@ -265,17 +346,10 @@ def process_patient_table(table, engine, args):
                 CRATE_COL_PK, CRATE_COL_RIO_NUMBER])
         execute(engine, args, """
             UPDATE {tablename} SET
-                {pk} = CAST(
-                    SUBSTRING(
-                        {rcep_mangled_pk},
-                        CHARINDEX('_', {rcep_mangled_pk}) + 1,
-                        LEN({rcep_mangled_pk}) - CHARINDEX('_', {rcep_mangled_pk})
-                    ) AS INTEGER
-                ),
+                -- pk is autogenerated as an INT IDENTITY field
                 {rio_number} = CAST({rcep_pt_id} AS INTEGER)
             WHERE
-                {pk} IS NULL
-                OR {rio_number} IS NULL
+                {rio_number} IS NULL
         """.format(  # noqa
             tablename=table.name,
             pk=CRATE_COL_PK,
@@ -283,20 +357,26 @@ def process_patient_table(table, engine, args):
             rio_number=CRATE_COL_RIO_NUMBER,
             rcep_pt_id=RCEP_COL_PATIENT_ID,
         ))
+        """ Chucked:
+            {pk} = CAST(
+                SUBSTRING(
+                    {rcep_mangled_pk},
+                    CHARINDEX('_', {rcep_mangled_pk}) + 1,
+                    LEN({rcep_mangled_pk}) - CHARINDEX('_', {rcep_mangled_pk})
+                ) AS INTEGER
+            ),
+        """
     # -------------------------------------------------------------------------
     # Add indexes, if absent
     # -------------------------------------------------------------------------
     # Note that the indexes are unlikely to speed up the WHERE NOT NULL search
     # above, so it doesn't matter that we add these last. Their use is for
     # the subsequent CRATE anonymisation table scans.
-    add_index_if_absent(engine, args, table, [
+    add_indexes(engine, args, table, [
         {
             'index_name': CRATE_IDX_PK,
             'column': CRATE_COL_PK,
-            'unique': args.rio,
-            # For RCEP (part of Document_ID), this key may be non-unique; see
-            # above (e.g. Care_Plan_Interventions).
-            # For RiO (SequenceID), this should be unique.
+            'unique': True,
         },
         {
             'index_name': CRATE_IDX_RIONUM,
@@ -305,12 +385,17 @@ def process_patient_table(table, engine, args):
     ])
 
 
+def drop_for_patient_table(table, engine, args):
+    drop_indexes(engine, args, table, [CRATE_IDX_PK, CRATE_IDX_RIONUM])
+    drop_columns(engine, args, table, [CRATE_COL_PK, CRATE_COL_RIO_NUMBER])
+
+
 # =============================================================================
 # Specific table processors
 # =============================================================================
 
 def process_master_patient_table(table, engine, args):
-    add_columns_if_absent(engine, args, table, {
+    add_columns(engine, args, table, {
         CRATE_COL_NHS_NUMBER: 'BIGINT',
     })
     if args.rcep:
@@ -332,16 +417,22 @@ def process_master_patient_table(table, engine, args):
     ))
 
 
+def drop_for_master_patient_table(table, engine, args):
+    drop_columns(engine, args, table, [CRATE_COL_NHS_NUMBER])
+
+
 def process_progress_notes(table, engine, args):
-    add_columns_if_absent(engine, args, table, {
-        'max_subnum_for_notenum': 'INTEGER',
-        'last_note_in_edit_chain': 'INTEGER',
+    add_columns(engine, args, table, {
+        CRATE_COL_MAX_SUBNUM: 'INTEGER',
+        CRATE_COL_LAST_NOTE: 'INTEGER',
     })
     # We're always in "RiO land", not "RCEP land", for this one.
-    add_index_if_absent(engine, args, table, [
+    add_indexes(engine, args, table, [
         {
             'index_name': CRATE_IDX_RIONUM_NOTENUM,
-            'column': 'rio_number, NoteNum',  # join index, for UPDATE below
+            'column': '{rio_number}, NoteNum'.format(
+                rio_number=CRATE_COL_RIO_NUMBER),
+            # join index, for UPDATE below
         },
     ])
 
@@ -349,7 +440,7 @@ def process_progress_notes(table, engine, args):
         "NoteNum", "SubNum", "EnteredInError", "EnteredInError"])
     if not args.print:
         ensure_columns_present(engine, table=table, column_names=[
-            "max_subnum_for_notenum", "last_note_in_edit_chain", "rio_number"])
+            CRATE_COL_MAX_SUBNUM, CRATE_COL_LAST_NOTE, CRATE_COL_RIO_NUMBER])
 
     # Find the maximum SubNum for each note, and store it.
     # Slow query, even with index.
@@ -357,40 +448,58 @@ def process_progress_notes(table, engine, args):
              "updating 'max_subnum_for_notenum'".format(table.name))
     execute(engine, args, """
         UPDATE p1
-        SET p1.max_subnum_for_notenum = subq.max_subnum
+        SET p1.{max_subnum_col} = subq.max_subnum
         FROM {tablename} p1 JOIN (
-            SELECT rio_number, NoteNum, MAX(SubNum) AS max_subnum
+            SELECT {rio_number}, NoteNum, MAX(SubNum) AS max_subnum
             FROM {tablename} p2
-            GROUP BY rio_number, NoteNum
+            GROUP BY {rio_number}, NoteNum
         ) subq
-        ON subq.rio_number = p1.rio_number
+        ON subq.{rio_number} = p1.{rio_number}
         AND subq.NoteNum = p1.NoteNum
-        WHERE p1.max_subnum_for_notenum IS NULL
-    """.format(tablename=table.name))
+        WHERE p1.{max_subnum_col} IS NULL
+    """.format(
+        max_subnum_col=CRATE_COL_MAX_SUBNUM,
+        tablename=table.name,
+        rio_number=CRATE_COL_RIO_NUMBER,
+    ))
 
     # Set a single column accordingly
     log.info("Progress notes table '{}': "
              "updating 'last_note_in_edit_chain'".format(table.name))
     execute(engine, args, """
         UPDATE {tablename} SET
-            last_note_in_edit_chain =
+            {last_note_col} =
                 CASE
-                    WHEN SubNum = max_subnum_for_notenum THEN 1
+                    WHEN SubNum = {max_subnum_col} THEN 1
                     ELSE 0
                 END
-        WHERE last_note_in_edit_chain IS NULL
-    """.format(tablename=table.name))
+        WHERE {last_note_col} IS NULL
+    """.format(
+        tablename=table.name,
+        last_note_col=CRATE_COL_LAST_NOTE,
+        max_subnum_col=CRATE_COL_MAX_SUBNUM,
+    ))
 
     # Create a view
     log.info("Creating view '{}'".format(VIEW_PROGRESS_NOTES_CURRENT))
-    execute(engine, args, """
-        CREATE VIEW {viewname} AS
-            SELECT *
-            FROM {tablename}
-            WHERE
-                (EnteredInError <> 1 OR EnteredInError IS NULL)
-                AND last_note_in_edit_chain = 1
-    """.format(viewname=VIEW_PROGRESS_NOTES_CURRENT, tablename=table.name))
+    select_sql = """
+        SELECT *
+        FROM {tablename}
+        WHERE
+            (EnteredInError <> 1 OR EnteredInError IS NULL)
+            AND {last_note_col} = 1
+    """.format(
+        tablename=table.name,
+        last_note_col=CRATE_COL_LAST_NOTE,
+    )
+    create_view(engine, args, VIEW_PROGRESS_NOTES_CURRENT, select_sql)
+
+
+def drop_for_progress_notes(table, engine, args):
+    drop_view(engine, args, VIEW_PROGRESS_NOTES_CURRENT)
+    drop_indexes(engine, args, table, [CRATE_IDX_RIONUM_NOTENUM])
+    drop_columns(engine, args, table, [CRATE_COL_MAX_SUBNUM,
+                                       CRATE_COL_LAST_NOTE])
 
 
 # =============================================================================
@@ -426,28 +535,27 @@ def add_postcode_geography_view(engine, args):
     log.info("Creating view '{}'".format(VIEW_ADDRESS_WITH_GEOGRAPHY))
     ensure_columns_present(engine, tablename=addresstable, column_names=[
         rio_postcodecol])
-    execute(engine, args, """
-        CREATE VIEW {viewname} AS
-            SELECT {origcols},
-                   {geogcols}
-            FROM {addresstable}
-            LEFT JOIN {pdb}.{pcdtab}
-            ON {addresstable}.{rio_postcodecol} = {pdb}.{pcdtab}.pcds
-            -- RCEP, and presumably RiO, appear to use the ONS pcds format, of
-            -- 2-4 char outward code; space; 3-char inward code.
-            -- If this fails, use this slower version:
-            -- ON REPLACE({addresstable}.{rio_postcodecol},
-            --            ' ',
-            --            '') = {pdb}.{pcdtab}.pcd_nospace
-    """.format(  # noqa
-        viewname=VIEW_ADDRESS_WITH_GEOGRAPHY,
+    select_sql = """
+        SELECT {origcols},
+               {geogcols}
+        FROM {addresstable}
+        LEFT JOIN {pdb}.{pcdtab}
+        ON {addresstable}.{rio_postcodecol} = {pdb}.{pcdtab}.pcds
+        -- RCEP, and presumably RiO, appear to use the ONS pcds format, of
+        -- 2-4 char outward code; space; 3-char inward code.
+        -- If this fails, use this slower version:
+        -- ON REPLACE({addresstable}.{rio_postcodecol},
+        --            ' ',
+        --            '') = {pdb}.{pcdtab}.pcd_nospace
+    """.format(
         addresstable=addresstable,
         origcols=", ".join(orig_column_specs),
         geogcols=", ".join(geog_col_specs),
         pdb=args.postcodedb,
         pcdtab=ONSPD_TABLE_POSTCODE,
         rio_postcodecol=rio_postcodecol,
-    ))
+    )
+    create_view(engine, args, VIEW_ADDRESS_WITH_GEOGRAPHY, select_sql)
 
 
 # =============================================================================
@@ -459,14 +567,26 @@ def process_table(table, engine, args):
     column_names = table.columns.keys()
     log.debug("TABLE: '{}'; COLUMNS: {}".format(tablename, column_names))
 
-    # Generic
-    if args.patient_table_indicator_column in column_names:
-        process_patient_table(table, engine, args)
-    # Specific
-    if tablename == args.master_patient_table:
-        process_master_patient_table(table, engine, args)
-    elif tablename == args.full_prognotes_table:
-        process_progress_notes(table, engine, args)
+    if args.drop_danger_drop:
+        # DROP STUFF! Different order
+        # Specific
+        if tablename == args.master_patient_table:
+            drop_for_master_patient_table(table, engine, args)
+        elif tablename == args.full_prognotes_table:
+            drop_for_progress_notes(table, engine, args)
+        # Generic
+        if args.patient_table_indicator_column in column_names:
+            drop_for_patient_table(table, engine, args)
+    else:
+        # CREATE STUFF!
+        # Generic
+        if args.patient_table_indicator_column in column_names:
+            process_patient_table(table, engine, args)
+        # Specific
+        if tablename == args.master_patient_table:
+            process_master_patient_table(table, engine, args)
+        elif tablename == args.full_prognotes_table:
+            process_progress_notes(table, engine, args)
 
 
 # =============================================================================
@@ -494,6 +614,9 @@ def main():
         "--rcep", action="store_true",
         help="Treat the source database as the product of Servelec's RiO CRIS "
              "Extract Program v2 (instead of raw RiO)")
+    parser.add_argument(
+        "--drop_danger_drop", action="store_true",
+        help="REMOVES new columns and indexes, rather than creating them.")
     parser.add_argument(
         "--cpft", action="store_true",
         help="Apply hacks for Cambridgeshire & Peterborough NHS Foundation "
@@ -544,6 +667,7 @@ def main():
     engine = create_engine(args.url, echo=args.echo, encoding=MYSQL_CHARSET)
     metadata.bind = engine
     log.info("Database: {}".format(repr(engine.url))) # ... repr hides password
+    log.debug("Dialect: {}".format(engine.dialect.name))
 
     metadata.reflect(engine)
 
@@ -551,7 +675,10 @@ def main():
                         key=lambda t: t.name.lower()):
         process_table(table, engine, args)
     if args.postcodedb:
-        add_postcode_geography_view(engine, args)
+        if args.drop_danger_drop:
+            drop_view(engine, args, VIEW_ADDRESS_WITH_GEOGRAPHY)
+        else:
+            add_postcode_geography_view(engine, args)
 
 
 if __name__ == '__main__':
