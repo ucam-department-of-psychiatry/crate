cd6a8c4f2d84afc5d72a59df13484c46d69c17fb
Rudolf Cardinal <rudolf@pobox.com>
2016-09-26 14:11:59 +0100

    pythonlib version increased to 0.2.4

--- setup.py
+++ setup.py
@@ -197,7 +197,7 @@ setup(
         # For the anonymiser/pythonlib:
         # ---------------------------------------------------------------------
 
-        'cardinal_pythonlib==0.2.3',
+        'cardinal_pythonlib==0.2.4',
 
         'beautifulsoup4==4.4.1',
         'prettytable==0.7.2',
