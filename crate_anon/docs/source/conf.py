30663e4bd03042c43631f6674cd889d7638e0cf8
Rudolf Cardinal <rudolf@pobox.com>
2018-06-20 00:32:46 +0100

    docs

--- crate_anon/docs/source/conf.py
+++ crate_anon/docs/source/conf.py
@@ -44,6 +44,7 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'sphinx.ext.viewcode',
     'sphinx.ext.todo',
     'sphinx.ext.githubpages',
     'sphinx.ext.autodoc',
