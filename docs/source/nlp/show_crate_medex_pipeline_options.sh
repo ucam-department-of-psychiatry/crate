81329b41aef9d2bdc72d746c05e5416d277b2abe
Martin Burchell <mb2353@cam.ac.uk>
2020-07-10 17:02:05 +0100

    Remove hardcoded MEDEX_DIR

--- docs/source/nlp/show_crate_medex_pipeline_options.sh
+++ docs/source/nlp/show_crate_medex_pipeline_options.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 CRATE_NLP_JAVA_CLASS_DIR=${CRATE_SOURCE_ROOT}/crate_anon/nlp_manager/compiled_nlp_classes
-MEDEX_DIR=~/dev/Medex_UIMA_1.3.6
 
 java \
     -classpath "${CRATE_NLP_JAVA_CLASS_DIR}":"${MEDEX_DIR}/bin":"${MEDEX_DIR}/lib/*" \
