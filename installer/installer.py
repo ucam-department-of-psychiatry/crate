224f11f3d7a7a9585c9c1312b188de9d87fa5b68
Martin Burchell <mb2353@cam.ac.uk>
2021-11-26 15:22:57 +0000

    Docker: Configure local settings

--- installer/installer.py
+++ installer/installer.py
@@ -2,6 +2,7 @@
 
 import os
 import sys
+import secrets
 from typing import Optional
 
 from prompt_toolkit.completion import PathCompleter
@@ -43,8 +44,20 @@ class Installer:
             self.get_docker_gate_bioyodie_resources_host_dir()
         )
         self.setenv(
+            "CRATE_DOCKER_CRATEWEB_CONFIG_FILENAME",
+            "crateweb_local_settings.py"
+        )
+        self.setenv(
             "CRATE_DOCKER_MYSQL_ROOT_PASSWORD",
             self.get_docker_mysql_root_password()
+        )
+        self.setenv(
+            "CRATE_DOCKER_MYSQL_CRATE_DATABASE_NAME",
+            "crate_web_db"
+        )
+        self.setenv(
+            "CRATE_DOCKER_MYSQL_CRATE_USER_NAME",
+            "crate_web_user"
         )
         self.setenv(
             "CRATE_DOCKER_MYSQL_CRATE_USER_PASSWORD",
@@ -140,18 +153,69 @@ class Installer:
         return value
 
     def create_local_settings(self) -> None:
-        os.chdir(DOCKERFILES_DIR)
+        if not os.path.exists(self.local_settings_full_path()):
+            os.chdir(DOCKERFILES_DIR)
 
-        demo_config_command = ("source /crate/venv/bin/activate; "
-                               "crate_print_demo_crateweb_config > "
-                               "$CRATE_WEB_LOCAL_SETTINGS")
+            demo_config_command = ("source /crate/venv/bin/activate; "
+                                   "crate_print_demo_crateweb_config > "
+                                   "$CRATE_WEB_LOCAL_SETTINGS")
 
-        docker.compose.run("crate_workers",
-                           remove=True,
-                           command=["/bin/bash",
-                                    "-c",
-                                    demo_config_command])
+            docker.compose.run("crate_workers",
+                               remove=True,
+                               command=["/bin/bash",
+                                        "-c",
+                                        demo_config_command])
 
+        self.configure_local_settings()
+
+    def configure_local_settings(self) -> None:
+        with open(self.local_settings_full_path(), "r") as f:
+            settings = f.read()
+
+        root_dir = "/crate"
+        config_dir = os.path.join(root_dir, "cfg")
+        archive_template_dir = os.path.join(config_dir, "archive_templates")
+        tmp_dir = os.path.join(root_dir, "tmp")
+        venv_dir = os.path.join(root_dir, "venv")
+        crate_install_dir = os.path.join(venv_dir, "lib", "python3.6",
+                                         "site-packages")
+
+        replace_dict = {
+            "archive_attachment_dir": os.path.join(config_dir,
+                                                   "archive_attachments"),
+            "archive_static_dir": os.path.join(archive_template_dir, "static"),
+            "archive_template_cache_dir": os.path.join(
+                tmp_dir, "archive_templates", "cache"
+            ),
+            "archive_template_dir": archive_template_dir,
+            "broker_url": "amqp://rabbitmq:5672",
+            "crate_install_dir": crate_install_dir,
+            "django_site_root_absolute_url": "http://crate_server:8088",
+            "mysql_db": os.getenv("CRATE_DOCKER_MYSQL_CRATE_DATABASE_NAME"),
+            "mysql_host": "crate_db",
+            "mysql_password": os.getenv(
+                "CRATE_DOCKER_MYSQL_CRATE_USER_PASSWORD"
+            ),
+            "mysql_port": "3306",
+            "mysql_user": os.getenv("CRATE_DOCKER_MYSQL_CRATE_USER_NAME"),
+            "pdf_logo_abs_url": "http://localhost/crate_logo",  # TODO
+            "private_file_storage_root": os.path.join(tmp_dir, "files"),
+            "secret_key": secrets.token_urlsafe(),
+
+        }
+
+        for (search, replace) in replace_dict.items():
+            settings = settings.replace(f"@@{search}@@", replace)
+
+        with open(self.local_settings_full_path(), "w") as f:
+            f.write(settings)
+
+
+    def local_settings_full_path(self) -> str:
+        return os.path.join(
+            os.getenv("CRATE_DOCKER_CONFIG_HOST_DIR"),
+            os.getenv("CRATE_DOCKER_CRATEWEB_CONFIG_FILENAME")
+        )
 
 def main() -> None:
     installer = Installer()
