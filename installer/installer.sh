5a7ba69d7ee82f398a9523d67cb4e58b7021fb67
Martin Burchell <mb2353@cam.ac.uk>
2021-11-29 11:49:07 +0000

    Docker: Remove update flags from installer - autodetect instead

--- installer/installer.sh
+++ installer/installer.sh
@@ -11,10 +11,6 @@ set -euxo pipefail
 # curl -L https://github.com/RudolfCardinal/crate/releases/latest/download/installer.sh | bash
 
 
-# TODO: Set to 1 when it is working
-UPDATE_PACKAGES=0
-UPDATE_VENV=0
-
 CRATE_HOME=${HOME}/crate
 CRATE_INSTALLER_VENV=${HOME}/.local/share/virtualenv/crate-installer
 
@@ -27,18 +23,14 @@ CRATE_INSTALLER_VENV=${HOME}/.local/share/virtualenv/crate-installer
 # INSTALLER_HOME = ${CRATE_HOME}/installer
 INSTALLER_HOME="$( cd "$( dirname "$0" )" && pwd )"
 
-if [ "$UPDATE_PACKAGES" -eq "1" ]; then
-    sudo apt-get update
-    sudo apt -y install python3-virtualenv python3-venv
-fi
+# Prerequisites (platform dependent) :
+# sudo apt-get update
+# sudo apt -y install python3-virtualenv python3-venv
 
 if [ ! -d "${CRATE_INSTALLER_VENV}" ]; then
     python3 -m venv ${CRATE_INSTALLER_VENV}
-fi
+    source ${CRATE_INSTALLER_VENV}/bin/activate
 
-source ${CRATE_INSTALLER_VENV}/bin/activate
-
-if [ "$UPDATE_VENV" -eq "1" ]; then
     python -m pip install -U pip setuptools
     python -m pip install -r ${INSTALLER_HOME}/installer-requirements.txt
 fi
