c3a0da8339b86984da379d7d79659d35f8c26bbb
Martin Burchell <mb2353@cam.ac.uk>
2021-11-30 07:03:55 +0000

    Docker: Temporarily dump the hosts file

--- installer/installer.py
+++ installer/installer.py
@@ -28,6 +28,12 @@ class Installer:
         self.create_directories()
         self.create_local_settings()
         self.create_anon_config()
+        print("Dumping hosts file")
+        print("==================")
+        docker.compose.run("crate_workers",
+                           remove=True,
+                           command=["/bin/bash", "-c", "cat /etc/hosts"])
+        print("==================")
         self.create_database()
 
     def configure(self) -> None:
