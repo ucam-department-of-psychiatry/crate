d01838f04ea104b0d7e291ee2b43b7ee6fbdecb4
Martin Burchell <mb2353@cam.ac.uk>
2022-01-04 11:02:31 +0000

    Bump Django to fix CVE-2021-44420

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ INSTALL_REQUIRES = [
     "cherrypy==18.6.0",  # Cross-platform web server
     "colorlog==4.1.0",  # colour in logs
     "distro==1.5.0",  # replaces platform.linux_distribution
-    "django==3.1.13",  # for main CRATE research database web server
+    "django==3.1.14",  # for main CRATE research database web server
     "django-debug-toolbar==3.0a2",  # Django debug toolbar
     # "django-debug-toolbar-template-profiler==2.0.1",  # v1.0.1 removed 2017-01-30: division by zero when rendering time is zero  # noqa
     "django-extensions==3.1.1",  # for graph_models, show_urls etc.
