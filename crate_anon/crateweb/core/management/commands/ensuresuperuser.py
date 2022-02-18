39de0aa5ee3f2b25e0c1b57ce8e58761788202b8
Martin Burchell <mb2353@cam.ac.uk>
2022-02-18 12:04:57 +0000

    Docker: Comment blocks

--- crate_anon/crateweb/core/management/commands/ensuresuperuser.py
+++ crate_anon/crateweb/core/management/commands/ensuresuperuser.py
@@ -1,3 +1,30 @@
+#!/usr/bin/env python
+
+"""
+crate_anon/crateweb/core/management/commands/ensuresuperuser.py
+
+===============================================================================
+
+    Copyright (C) 2015-2021 Rudolf Cardinal (rudolf@pobox.com).
+
+    This file is part of CRATE.
+
+    CRATE is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CRATE is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CRATE. If not, see <https://www.gnu.org/licenses/>.
+
+===============================================================================
+"""
+
 import os
 
 from django.contrib.auth import get_user_model
--- installer/installer.sh
+++ installer/installer.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-set -euxo pipefail
+# installer/installer.sh
 
+# ==============================================================================
+#
+#     Copyright (C) 2015-2021 Rudolf Cardinal (rudolf@pobox.com).
+#
+#     This file is part of CRATE.
+#
+#     CRATE is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, either version 3 of the License, or
+#     (at your option) any later version.
+#
+#     CRATE is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#     GNU General Public License for more details.
+#
+#     You should have received a copy of the GNU General Public License
+#     along with CRATE. If not, see <https://www.gnu.org/licenses/>.
+#
+# ==============================================================================
+
+# Installs CRATE running under Docker with demonstration databases.
 # Do as little as possible in this script.
-# This should just bootstrap installer.py.
+# Do as much as possible in installer.py.
+
+set -euxo pipefail
 
 # Prerequisites for Windows:
 # Install WSL2
