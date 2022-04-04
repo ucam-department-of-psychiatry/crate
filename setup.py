9a92c5642fa9100a52a0528deac8e2fc2f0292b5
Martin Burchell <mb2353@cam.ac.uk>
2022-04-04 11:41:35 +0100

    Add Black auto formatter

--- setup.py
+++ setup.py
@@ -131,6 +131,7 @@ INSTALL_REQUIRES = [
     "zope.sqlalchemy==1.3",  # Zope/SQLAlchemy transaction integration
 
     # For development only:
+    "black==22.3.0",  # auto code formatter
     "flake8==3.8.4",  # code checks
     "docutils==0.17",  # documentation, 0.18 not compatible with Sphinx
 
