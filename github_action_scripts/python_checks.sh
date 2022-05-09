0784848f476ae9eb101d430d32385a088db1bb68
Martin Burchell <mb2353@cam.ac.uk>
2022-05-09 16:59:22 +0100

    Move bulk of GitHub action scripts from .yaml into own files

---
+++ github_action_scripts/python_checks.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+# Run from .github/workflows/python-checks.yml
+# Install CRATE python packages and run various tests
+
+set -eux -o pipefail
+
+cd ${GITHUB_WORKSPACE}
+python -m venv ${HOME}/venv
+source ${HOME}/venv/bin/activate
+python -VV
+python -m site
+python -m pip install -U pip
+echo dumping pre-installed packages
+python -m pip freeze
+echo installing pip packages
+python -m pip install -e .
+echo checking packages for conflicts
+python -m pip check
+echo installing vulnerability checker
+python -m pip install safety
+echo checking packages for vulnerabilities
+# All of these vulnerabilities look either harmless or very low risk
+# 44715 numpy fixed in 1.22 https://github.com/numpy/numpy/issues/19038
+# 44716 numpy fixed in 1.22 https://github.com/numpy/numpy/issues/19000
+# 44717 numpy fixed in 1.22 https://github.com/numpy/numpy/issues/18993
+safety check --full-report -i 44715 -i 44716 -i 44717
+crate_anon_web_create_private_settings
+echo checking python formatting
+black --line-length 79 --diff --check .
+echo checking python for style and errors
+flake8 --config=setup.cfg .
+echo running tests
+pytest -v
