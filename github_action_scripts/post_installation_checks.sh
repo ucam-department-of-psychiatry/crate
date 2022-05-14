fd42fb98eae0ebe4636532505a0559e5814278e4
Martin Burchell <mb2353@cam.ac.uk>
2022-05-14 07:21:56 +0100

    Test the anonymisation API in the installer action

--- github_action_scripts/post_installation_checks.sh
+++ github_action_scripts/post_installation_checks.sh
@@ -17,3 +17,6 @@ cd ${CRATE_HOME}/docker/dockerfiles
 # Check anonymiser worked
 anonymised=$(docker compose exec -T research_db mysql -Ns -u research -presearch research -e "SELECT SUBSTR(note, 1, 36) FROM note LIMIT 1")
 [ "${anonymised}" = "\n[__PPP__] [__PPP__] lived on a farm" ]
+# Check anonymisation API is working
+anonymised=$(curl -L --fail -X POST http://localhost:8088/crate/anon_api/scrub/ -H 'Content-Type: application/json' -d '{"text": {"test": "He was born on 1st January 1970"},"patient": {"dates": ["1970-01-01"]}}' | jq --raw-output '.anonymised.test')
+[ "${anonymised}" == "He was born on [__PPP__]" ]
