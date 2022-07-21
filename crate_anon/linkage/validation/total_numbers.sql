ca436104653ad89553f2f8a03909061ad23551a6
Rudolf Cardinal <rudolf@pobox.com>
2022-07-21 10:30:54 +0100

    Add query for total distinct patient count.

--- crate_anon/linkage/validation/total_numbers.sql
+++ crate_anon/linkage/validation/total_numbers.sql
@@ -33,3 +33,42 @@ SELECT
     COUNT(*) AS total_n,
     SUM(IIF(DOB IS NULL, 1, 0)) AS n_with_no_dob
 FROM SystmOne.dbo.S1_Patient  -- 619062
+
+
+-- ============================================================================
+-- Total number of distinct NHS numbers, in any of the four databases
+-- ============================================================================
+-- Ignore test numbers for now (they are few or non-existent).
+
+SELECT COUNT(DISTINCT nhs_number) AS n_distinct_patients_with_nhs_number
+FROM (
+    -- CDL
+    SELECT TRY_CAST(REPLACE(c.NHS_ID, ' ', '') AS BIGINT) AS nhs_number
+    FROM zVaultCRS_CDL.dbo.NHSID_BRC_Lookup AS c
+    WHERE LEFT(c.NHS_ID, 3) != '999'
+
+    UNION ALL
+
+    -- PCMIS
+    SELECT TRY_CAST(p.NHSNumber AS BIGINT) AS nhs_number
+    FROM rawPCMIS.dbo.PatientDetails AS p
+    WHERE LEFT(p.NHSNumber, 3) != '999'
+
+    UNION ALL
+
+    -- RiO
+    -- Empirically: no spaces.
+    SELECT TRY_CAST(r.NNN AS BIGINT) AS nhs_number
+    FROM RiO62CAMLive.dbo.Client AS r
+    WHERE LEFT(r.NNN, 3) != '999'
+
+    UNION ALL
+
+    -- SystmOne
+    -- Empirically: no spaces.
+    SELECT TRY_CAST(s.NHSNumber AS BIGINT) AS nhs_number
+    FROM SystmOne.dbo.S1_Patient AS s
+    WHERE LEFT(s.NHSNumber, 3) != '999'
+
+) AS subquery
+-- 756821 on 2022-07-21.
