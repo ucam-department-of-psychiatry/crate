a66d7e894d7a0168b714aec67f461184d532262a
Martin Burchell <mb2353@cam.ac.uk>
2023-05-22 10:39:33 +0100

    Only run anonymisation checks in workflow with demo database

--- github_action_scripts/post_installation_checks.sh
+++ github_action_scripts/post_installation_checks.sh
@@ -14,9 +14,3 @@ curl -I -L --retry 10 --fail --insecure "${SERVER_IP}:8000/crate/"
 # Check static files collected
 curl -I -L --fail --insecure "${SERVER_IP}:8000/crate_static/scrubber.png"
 cd "${CRATE_HOME}/docker/dockerfiles"
-# Check anonymiser worked
-anonymised=$(docker compose exec -T research_db mysql -Ns -u research -presearch research -e "SELECT SUBSTR(note, 1, 36) FROM note LIMIT 1")
-[ "${anonymised}" = "\n[__PPP__] [__PPP__] lived on a farm" ]
-# Check anonymisation API is working
-anonymised=$(curl -L --fail -X POST $SERVER_IP:8000/crate/anon_api/scrub/ -H 'Content-Type: application/json' -d '{"text": {"test": "He was born on 1st January 1970"},"patient": {"dates": ["1970-01-01"]}}' | jq --raw-output '.anonymised.test')
-[ "${anonymised}" == "He was born on [__PPP__]" ]
