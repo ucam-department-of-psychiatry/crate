9cf4dd5e73ada700bcdea01fe693b9811d894c50
Martin Burchell <mb2353@cam.ac.uk>
2023-06-06 14:31:40 +0100

    Create more realistic-looking patients in the demo database

--- github_action_scripts/post_installation_checks.sh
+++ github_action_scripts/post_installation_checks.sh
@@ -15,8 +15,8 @@ curl -I -L --retry 10 --fail "${SERVER_IP}:8000/crate/"
 curl -I -L --fail "${SERVER_IP}:8000/crate_static/scrubber.png"
 cd "${CRATE_HOME}/docker/dockerfiles"
 # Check anonymiser worked
-anonymised=$(docker compose exec -T research_db mysql -Ns -u research -presearch research -e "SELECT SUBSTR(note, 1, 36) FROM note LIMIT 1")
-[ "${anonymised}" = "\n[__PPP__] [__PPP__] lived on a farm" ]
+anonymised=$(docker compose exec -T research_db mysql -Ns -u research -presearch research -e "SELECT SUBSTR(note, 1, 37) FROM note LIMIT 1")
+[ "${anonymised}" = "\nI saw [__PPP__] [__PPP__] on 20000101" ]
 # Check anonymisation API is working
 anonymised=$(curl -L --fail -X POST $SERVER_IP:8000/crate/anon_api/scrub/ -H 'Content-Type: application/json' -d '{"text": {"test": "He was born on 1st January 1970"},"patient": {"dates": ["1970-01-01"]}}' | jq --raw-output '.anonymised.test')
 [ "${anonymised}" == "He was born on [__PPP__]" ]
