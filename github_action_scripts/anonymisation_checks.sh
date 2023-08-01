dbc3619e18be8e317a88311db76175b5ded26ca2
Martin Burchell <mb2353@cam.ac.uk>
2023-08-01 14:37:40 +0100

    Attempt to fix post-installer check in GitHub workflow
    Use curl --insecure with https

--- github_action_scripts/anonymisation_checks.sh
+++ github_action_scripts/anonymisation_checks.sh
@@ -11,5 +11,5 @@ SERVER_IP=$(docker inspect crate_crate_server --format='{{range .NetworkSettings
 anonymised=$(docker compose exec -T research_db mysql -Ns -u research -presearch research -e "SELECT SUBSTR(note, 1, 37) FROM note LIMIT 1")
 [ "${anonymised}" = "I saw [__PPP__] [__PPP__] on 20000101" ]
 # Check anonymisation API is working
-anonymised=$(curl -L --fail -X POST $SERVER_IP:8000/crate/anon_api/scrub/ -H 'Content-Type: application/json' -d '{"text": {"test": "He was born on 1st January 1970"},"patient": {"dates": ["1970-01-01"]}}' | jq --raw-output '.anonymised.test')
+anonymised=$(curl -L --fail --insecure -X POST https://$SERVER_IP:8000/crate/anon_api/scrub/ -H 'Content-Type: application/json' -d '{"text": {"test": "He was born on 1st January 1970"},"patient": {"dates": ["1970-01-01"]}}' | jq --raw-output '.anonymised.test')
 [ "${anonymised}" == "He was born on [__PPP__]" ]
