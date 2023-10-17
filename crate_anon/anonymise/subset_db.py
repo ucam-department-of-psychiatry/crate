fdfbc7a7328d18db18ae69afa40188358c6a9405
Martin Burchell <mb2353@cam.ac.uk>
2023-10-17 17:51:28 +0100

    Fix copy-paste error in comment block

--- crate_anon/anonymise/subset_db.py
+++ crate_anon/anonymise/subset_db.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 """
-crate_anon/anonymise/researcher_report.py
+crate_anon/anonymise/subset_db.py
 
 ===============================================================================
 
