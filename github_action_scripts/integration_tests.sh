91a0561b68b2303c5308af1ee90d9783b35f3905
Martin Burchell <mb2353@cam.ac.uk>
2024-02-01 12:03:38 +0000

    We might need freetds-dev as well
    fatal error: sqlfront.h: No such file or directory

--- github_action_scripts/integration_tests.sh
+++ github_action_scripts/integration_tests.sh
@@ -23,7 +23,7 @@ sudo apt-get -y install fontconfig libxrender1 xfonts-75dpi xfonts-base
 sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
 
 # Needed for pymssql from source
-sudo apt-get -y install libkrb5-dev
+sudo apt-get -y install libkrb5-dev freetds-dev
 PYTHON="${HOME}/venv/bin/python"
 # Same versions as in docker/dockerfiles/crate.Dockerfile
 ${PYTHON} -m pip install --no-binary pymssql mssql-django==1.2 mysqlclient==1.4.6 psycopg2==2.8.5 pyodbc==4.0.30 pymssql==2.2.11
