8b08ac9d626dc3e18dbe2b2fc67be0f507cab5dd
Martin Burchell <mb2353@cam.ac.uk>
2024-02-01 11:50:31 +0000

    Try installing pymssql from source
    Experimental change to see if that fixes this error:
    Login failed for user 'administrator'
    https://stackoverflow.com/questions/10121212/cannot-connect-to-sql-server-database-using-pymssql-but-can-connect-using-underl
    suggests that the binary does not support encryption

--- github_action_scripts/integration_tests.sh
+++ github_action_scripts/integration_tests.sh
@@ -24,7 +24,7 @@ sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
 
 PYTHON="${HOME}/venv/bin/python"
 # Same versions as in docker/dockerfiles/crate.Dockerfile
-${PYTHON} -m pip install mssql-django==1.2 mysqlclient==1.4.6 psycopg2==2.8.5 pyodbc==4.0.30 pymssql==2.2.11
+${PYTHON} -m pip install --no-binary pymssql mssql-django==1.2 mysqlclient==1.4.6 psycopg2==2.8.5 pyodbc==4.0.30 pymssql==2.2.11
 ${PYTHON} ${GITHUB_WORKSPACE}/crate_anon/integration_tests/test_workflow.py --engine ${ENGINE} startengine
 ENGINE_IP=$(docker inspect crate_test_container_engine --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
 wait-for-it "${ENGINE_IP}:${PORT}" --timeout=300
