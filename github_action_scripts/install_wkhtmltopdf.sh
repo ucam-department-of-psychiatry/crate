2cbd0a6229e60a6b0969d2c94a742d9335c35263
Martin Burchell <mb2353@cam.ac.uk>
2024-02-17 07:59:17 +0000

    Missing script to install wkhtmltopdf

---
+++ github_action_scripts/install_wkhtmltopdf.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+# Install wkhtmltopdf on headless ubuntu 18 vps
+# https://gist.github.com/lobermann/ca0e7bb2558b3b08923c6ae2c37a26ce
+# 429 = Too many requests. Unfortunately wget doesn't read the
+# Retry-after header so just wait 5 minutes
+wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
+sudo apt-get -y install fontconfig libxrender1 xfonts-75dpi xfonts-base
+sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
