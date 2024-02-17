a58d5702754bdf5cf2fd014682ea10eee9405e5b
Martin Burchell <mb2353@cam.ac.uk>
2024-02-17 07:58:31 +0000

    Separate installation of wkhtmltopdf in workflows

--- github_action_scripts/integration_tests.sh
+++ github_action_scripts/integration_tests.sh
@@ -14,13 +14,6 @@ ENGINE=$1
 PORT=$2
 
 sudo apt -y install wait-for-it
-# Install wkhtmltopdf on headless ubuntu 18 vps
-# https://gist.github.com/lobermann/ca0e7bb2558b3b08923c6ae2c37a26ce
-# 429 = Too many requests. Unfortunately wget doesn't read the
-# Retry-after header so just wait 5 minutes
-wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-sudo apt-get -y install fontconfig libxrender1 xfonts-75dpi xfonts-base
-sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
 
 PYTHON="${HOME}/venv/bin/python"
 # Same versions as in docker/dockerfiles/crate.Dockerfile
