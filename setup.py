d678be76dc59d21fc8bd802b876ba6539b25676a
Martin Burchell <mb2353@cam.ac.uk>
2024-02-19 15:33:11 +0000

    Use consistent Black versions

--- setup.py
+++ setup.py
@@ -143,7 +143,7 @@ INSTALL_REQUIRES = [
     # -------------------------------------------------------------------------
     # For development only:
     # -------------------------------------------------------------------------
-    "black==22.3.0",  # auto code formatter
+    "black==22.8.0",  # auto code formatter
     "factory_boy==3.3.0",  # easier test data creation
     "faker==13.3.1",  # test data creation
     "flake8==3.8.4",  # code checks
