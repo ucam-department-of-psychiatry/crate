a1d1ba1e56451c7d6fdf4b257319a6b3bfbd3049
Martin Burchell <mb2353@cam.ac.uk>
2024-02-19 14:32:23 +0000

    Missing bracket

--- github_action_scripts/python_tests.sh
+++ github_action_scripts/python_tests.sh
@@ -7,7 +7,7 @@ set -eux -o pipefail
 
 VENV_BIN="${HOME}/venv/bin"
 PYTHON="${VENV_BIN}/python"
-PYTEST="${VENV_BIN/pytest"
+PYTEST="${VENV_BIN}/pytest"
 
 ${PYTHON} -m pip install mysqlclient
 cd "${GITHUB_WORKSPACE}"
