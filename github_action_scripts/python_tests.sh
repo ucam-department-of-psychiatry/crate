5fe29b8be710b2a15d59cea6f2630b7615681c08
Martin Burchell <mb2353@cam.ac.uk>
2024-02-20 15:11:11 +0000

    Fix pytest options for sqlite

--- github_action_scripts/python_tests.sh
+++ github_action_scripts/python_tests.sh
@@ -5,51 +5,51 @@
 
 set -euxo pipefail
 
-if [ "$#" != "2" ]; then
-   echo "Usage: $0 <mysql|postgres|sqlite|sqlserver> <port>"
-   exit 1
+ENGINE=$1
+if [ "${ENGINE}" == "" ]; then
+   echo "Usage: $0 <mysql|postgres|sqlite|sqlserver> [port]"
+    exit 1
 fi
 
-ENGINE=$1
-PORT=$2
+if [ "${ENGINE}" == "sqlite" ]; then
+    DB_OPTION=""
+else
+    QUERY=""
+    PORT=$2
 
-VENV_BIN="${HOME}/venv/bin"
-PYTHON="${VENV_BIN}/python"
-PYTEST="${VENV_BIN}/pytest"
+    if [ "${PORT}" == "" ]; then
+        echo "You must specify a port for engine ${ENGINE}"
+        exit 1
+    fi
 
-${PYTHON} -m pip install mysqlclient
-cd "${GITHUB_WORKSPACE}"/crate_anon
-echo running tests
-export CRATE_RUN_WITHOUT_LOCAL_SETTINGS=True
-export CRATE_NLP_WEB_CONFIG=${GITHUB_WORKSPACE}/github_action_scripts/test_nlp_web_config.ini
+    case ${ENGINE} in
+        mysql)
+            SCHEME="mysql+mysqldb"
+            QUERY="?charset=utf8"
+            ;;
 
-ENGINE_IP=$(docker inspect crate_test_container_engine --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
-TEST_USER=tester
-TEST_PASSWORD=QcigecuW1myo
-TEST_DB=testdb
+        postgres)
+            SCHEME="postgresql"
+            ;;
 
-QUERY=""
+        sqlserver)
+            SCHEME="mssql+pymssql"
+            ;;
+    esac
 
-case ${ENGINE} in
-    mysql)
-        SCHEME="mysql+mysqldb"
-        QUERY="?charset=utf8"
-        ;;
-
-    postgres)
-        SCHEME="postgresql"
-        ;;
-
-    sqlserver)
-        SCHEME="mssql+pymssql"
-        ;;
-esac
-
-DB_OPTION=""
-
-if [ "${ENGINE}" != "sqlite" ]; then
+    ENGINE_IP=$(docker inspect crate_test_container_engine --format='{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}')
+    TEST_USER=tester
+    TEST_PASSWORD=QcigecuW1myo
+    TEST_DB=testdb
     DB_OPTION="--db-url ${SCHEME}://${TEST_USER}:${TEST_PASSWORD}@${ENGINE_IP}:${PORT}/${TEST_DB}${QUERY}"
 fi
 
+echo running tests
+cd "${GITHUB_WORKSPACE}"/crate_anon
+export CRATE_RUN_WITHOUT_LOCAL_SETTINGS=True
+export CRATE_NLP_WEB_CONFIG=${GITHUB_WORKSPACE}/github_action_scripts/test_nlp_web_config.ini
+
+VENV_BIN="${HOME}/venv/bin"
+PYTEST="${VENV_BIN}/pytest"
 
 ${PYTEST} -v ${DB_OPTION}
