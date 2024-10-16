5a3a5698c76f04f42bbd0764f59da36455edaf98
Martin Burchell <mb2353@cam.ac.uk>
2024-10-16 15:43:15 +0100

    Bump numpy for later Python support

--- setup.py
+++ setup.py
@@ -98,7 +98,7 @@ INSTALL_REQUIRES = [
     # mmh3 requires VC++
     "mmh3==2.5.1",  # MurmurHash, for fast non-cryptographic hashing; optionally used by cardinal_pythonlib; requires VC++ under Windows?  # noqa: E501
     "numba==0.60.0",  # just-in-time compilation of functions
-    "numpy==1.22.4",  # numerical work
+    "numpy==1.26.4",  # numerical work
     "openpyxl==3.0.7",  # read Excel
     "ordered-set==4.1.0",  # ordered sets; search for ordered_set
     "pendulum==2.1.2",  # dates/times
