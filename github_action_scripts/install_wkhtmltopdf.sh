ab1668eb867c38c95d288d0c9094dd3c99505787
Rudolf Cardinal <rudolf@pobox.com>
2025-01-08 17:27:17 +0000

    try to fix wkhtmltopdf installer by using gdebi not dpkg -i

--- github_action_scripts/install_wkhtmltopdf.sh
+++ github_action_scripts/install_wkhtmltopdf.sh
@@ -5,5 +5,5 @@
 # 429 = Too many requests. Unfortunately wget doesn't read the
 # Retry-after header so just wait 5 minutes
 wget --retry-on-http-error=429 --waitretry=300 --tries=20 https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.bionic_amd64.deb
-sudo apt-get -y install fontconfig libxrender1 xfonts-75dpi xfonts-base
-sudo dpkg -i wkhtmltox_0.12.6-1.bionic_amd64.deb
+sudo apt-get -y install fontconfig gdebi libxrender1 xfonts-75dpi xfonts-base
+sudo gdebi --non-interactive wkhtmltox_0.12.6-1.bionic_amd64.deb
