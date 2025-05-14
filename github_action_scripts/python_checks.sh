a0e7977512ff66eb804c2f90d92ae62be89baf67
Martin Burchell <mb2353@cam.ac.uk>
2025-05-14 10:48:55 +0100

    No longer need to skip the sqlalchemy safety check
    now we're on 2.0

--- github_action_scripts/python_checks.sh
+++ github_action_scripts/python_checks.sh
@@ -18,12 +18,10 @@ echo installing vulnerability checker
 ${PYTHON} -m pip install safety
 echo checking packages for vulnerabilities
 # All of these vulnerabilities look either harmless or very low risk
-# 51668 sqlalchemy fix in 2.0 beta, we don't log Engine.URL()
-#       https://github.com/sqlalchemy/sqlalchemy/issues/8567
 # 67599 pip. Disputed and only relevant if using --extra-index-url,
 #       which we're not.
 # 70612 jinja2. The maintainer and multiple third parties
 #       believe that this vulnerability isn't valid because
 #       users shouldn't use untrusted templates without
 #       sandboxing.
-${SAFETY} check --full-report --ignore=51668 --ignore=67599 --ignore=70612
+${SAFETY} check --full-report --ignore=67599 --ignore=70612
