aa80b3885db78f40c85c9c9b76f64536afb6dc21
Martin Burchell <mb2353@cam.ac.uk>
2025-06-25 11:54:50 +0100

    Bump requests to fix CVE-2024-47081

--- setup.py
+++ setup.py
@@ -131,7 +131,7 @@ INSTALL_REQUIRES = [
     "pyramid==1.10.4",  # Pyramid web framework
     "pyramid_tm==2.4",  # Pyramid transaction management
     "redis==4.5.4",  # interface to Redis in-memory key-value database
-    "requests==2.32.2",  # HTTP requests
+    "requests==2.32.4",  # HTTP requests
     "tornado==6.5",  # web framework
     "transaction==3.0.0",  # generic transaction management
     "urllib3==2.3.0",  # used by requests
