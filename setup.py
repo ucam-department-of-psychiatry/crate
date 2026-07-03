5283a30c4eb17550a68860c52655137eded0f436
Martin Burchell <mb2353@cam.ac.uk>
2026-07-03 21:27:07 +0100

    Bump cryptography to fix GHSA-537c-gmf6-5ccf

--- setup.py
+++ setup.py
@@ -126,7 +126,7 @@ INSTALL_REQUIRES = [
     # Packages for cloud NLP:
     # -------------------------------------------------------------------------
     "bcrypt==3.2.2",  # bcrypt encryption
-    "cryptography==46.0.7",  # cryptography library
+    "cryptography==48.0.1",  # cryptography library
     # "mysqlclient",  # database access
     "paste==3.4.2",  # middleware; https://github.com/cdent/paste/
     "pyramid==1.10.8",  # Pyramid web framework
